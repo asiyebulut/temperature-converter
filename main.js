cb63b7b81782a8731c970ad7efca0c4990a5b5de
asiyebulut <atubakesenel@gmail.com>
2021-03-02 13:39:51 -0800

    ready to convert between F and C

---
+++ main.js
@@ -0,0 +1,9 @@
+var celsiusTemp = 25;
+console.log(celsiusTemp);
+var convertCToF = (celsiusTemp * 9) / 5 + 32;
+console.log(celsiusTemp + '\u00B0' + 'C is ' + convertCToF + '\u00B0' + 'F.');
+
+var fahrenheitTemp = 77;
+console.log(fahrenheitTemp);
+var convertFToC = (fahrenheitTemp - 32) * 5 / 9;
+console.log(fahrenheitTemp + '\u00B0' + 'C is ' + convertFToC + '\u00B0' + 'F.');
